f081b1741904b53a4a964644840ed4de00f0aefd
Chris Johnson <chris84948@gmail.com>
2021-04-27 16:05:06 -0500

    Added a little error checking to stop null exceptions

--- android/src/main/java/com/chrisbjohnson/fluttercalendarplugin/FlutterCalendarPlugin.java
+++ android/src/main/java/com/chrisbjohnson/fluttercalendarplugin/FlutterCalendarPlugin.java
@@ -69,9 +69,11 @@ public class FlutterCalendarPlugin implements MethodCallHandler, PluginRegistry.
     // If request is cancelled, the result arrays are empty.
     if (grantResults.length > 0
             && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-      _permissionCallback.granted(requestCode);
+      if (_permissionCallback != null)
+        _permissionCallback.granted(requestCode);
     } else {
-      _permissionCallback.denied();
+      if (_permissionCallback != null)
+        _permissionCallback.denied();
     }
 
     return true;
